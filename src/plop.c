1ffa86676945098564115fa52b685ffd4bb44ba1
Rémi Papillié <remi.papillie@gmail.com>
2015-04-26 13:59:28 +0200

    organized data for the two songs

--- src/plop.c
+++ src/plop.c
@@ -78,13 +78,13 @@ static void menu_update()
 			{
 				menu_leave();
 				current_state = STATE_GAME;
-				game_enter("data/json/test_patterns.json");
+				game_enter("data/json/danse.json");
 			}
 			if ((x >= 4) && (x <= 5))
 			{
 				menu_leave();
 				current_state = STATE_GAME;
-				game_enter("data/json/test_patterns.json");
+				game_enter("data/json/folk.json");
 			}
 		}
 	}
